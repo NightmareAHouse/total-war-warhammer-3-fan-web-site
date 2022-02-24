493399a1888f865c59b53c0a417b1cea7caa0263
nightmareahouse <nightmareahouse@gmail.com>
2022-02-24 19:41:47 +0200

    Add code

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/App.css';
+import Header from "../component/head-component/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
